4938b28277219d25878799d8c930e15f98c7b791
dyhwang7 <daeyoung94@gmail.com>
2021-04-07 20:02:53 -0700

    html setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace daeyo
 {
@@ -6,7 +12,20 @@ namespace daeyo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World     !");
+            GetHtmlAsync();
+
+        }
+
+        private static async void GetHtmlAsync()
+        {
+            var url = "https://finance.yahoo.com/quote/AAPL/";
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            
         }
     }
 }
